915c92e91a7a09fa1bba513a29b74363b304249d
lightonthefloor <1241333524@qq.com>
2022-08-10 12:07:38 +0800

    Input added

---
+++ Input.h
@@ -0,0 +1,17 @@
+//
+// Created by Trisoil on 2022/8/10.
+//
+#include "cstdio"
+
+#ifndef GIT_TEST_INPUT_H
+#define GIT_TEST_INPUT_H
+
+void input()
+{
+    char a[5005];
+    scanf("%s",a);
+    printf("Hello %s\n",a);
+}
+
+
+#endif //GIT_TEST_INPUT_H
